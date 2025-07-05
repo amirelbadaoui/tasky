91f7bda3241e6b68796d54819658fd58772420df
Amir Elbadaoui <amireb51@gmail.com>
2025-07-05 19:47:11 -0400

    Implemented create task list functionality

--- src/main/java/com/soghiar/tasky/controllers/TaskListController.java
+++ src/main/java/com/soghiar/tasky/controllers/TaskListController.java
@@ -1,11 +1,10 @@
 package com.soghiar.tasky.controllers;
 
 import com.soghiar.tasky.domain.dto.TaskListDto;
+import com.soghiar.tasky.domain.entities.TaskList;
 import com.soghiar.tasky.mapper.TaskListMapper;
 import com.soghiar.tasky.services.TaskListService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,6 +26,13 @@ public class TaskListController {
                 .stream()
                 .map(taskListMapper::toDto)
                 .toList();
+    }
+
+    @PostMapping
+    public TaskListDto createTaskList(@RequestBody TaskListDto taskListDto) {
+        TaskList createdTaskList = taskListService.createTaskList(taskListMapper.fromDto(taskListDto));
+
+        return taskListMapper.toDto(createdTaskList);
     }
 
 }
--- src/main/java/com/soghiar/tasky/services/impl/TaskListServiceImpl.java
+++ src/main/java/com/soghiar/tasky/services/impl/TaskListServiceImpl.java
@@ -6,6 +6,7 @@ import com.soghiar.tasky.repositories.TaskListRepository;
 import com.soghiar.tasky.services.TaskListService;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -20,5 +21,21 @@ public class TaskListServiceImpl implements TaskListService {
     @Override
     public List<TaskList> listTaskLists() {
         return taskListRepository.findAll();
+    }
+
+    @Override
+    public TaskList createTaskList(TaskList taskList) {
+        if(null != taskList.getId()) throw new IllegalArgumentException("Task list already has an id");
+        if(null == taskList.getTitle() || taskList.getTitle().isBlank()) throw new IllegalArgumentException("Task list title must be present!");
+
+        LocalDateTime now = LocalDateTime.now();
+        return taskListRepository.save(new TaskList(
+                null,
+                taskList.getTitle(),
+                taskList.getDescription(),
+                null,
+                now,
+                now
+        ));
     }
 }
