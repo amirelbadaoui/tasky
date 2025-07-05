b30b6a36d07fbd811821f6625479300c5d3a29e8
Amir Elbadaoui <amireb51@gmail.com>
2025-07-05 12:24:04 -0400

    Created Task & TaskList Repositories

---
+++ src/main/java/com/soghiar/tasky/repositories/TaskListRepository.java
@@ -0,0 +1,11 @@
+package com.soghiar.tasky.repositories;
+
+import com.soghiar.tasky.domain.entities.TaskList;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface TaskListRepository extends JpaRepository<TaskList, UUID> {
+}
